e8e314081fb009b0c7f64ea9351b6b7989bdb10d
guxianwang <guxw@njzhyl.cn>
2022-03-14 15:29:36 +0800

    feat:dateRange组件调整

--- src/datetime-range/index.vue
+++ src/datetime-range/index.vue
@@ -2,19 +2,18 @@
   <div class="datetime-range">
     <div class="datetime-range-select">
       <datetime-select
-        v-model="startDate"
+        v-model="datetimeRange.startDate"
         :label="startDateLabel"
         :display-format="displayFormat"
         :type="type"
-        :is-link="isLink"
+        :is-link="false"
         :rules="rules"
-        :readonly="readonly"
         :value-format="valueFormat"
         :min-date="rangeMinDate"
         :max-date="maxDate"
         :placeholder="startDatePlaceholder"
         :right-icon="rightIcon"
-        @confirm="onConfirm"
+        @change="changeStart"
       />
     </div>
     <slot
@@ -27,48 +26,48 @@
     </slot>
     <div class="datetime-range-select">
       <datetime-select
-        v-model="endDate"
+        v-model="datetimeRange.endDate"
         :label="endDateLabel"
         :display-format="displayFormat"
         :placeholder="endDatePlaceholder"
         :value-format="valueFormat"
         :type="type"
-        :readonly="readonly"
         :min-date="minDate"
         :max-date="rangeMaxDate"
         :rules="rules"
-        :is-link="isLink"
+        :is-link="false"
         :right-icon="rightIcon"
-        @confirm="onConfirm"
+        @change="changeEnd"
       />
     </div>
   </div>
 </template>
 
 <script>
-import DatettimeSelect from '../datetime-select';
+import { logger, clone } from '@njzhyl/common-util';
+import { DatetimeSelect } from '@njzhyl/common-component-vant';
 import dayjs from 'dayjs';
 
 export default {
-  name: 'datetime-range',
+  name: 'DatetimeRange',
   components: {
-    [DatettimeSelect.name]: DatettimeSelect,
+    [DatetimeSelect.name]: DatetimeSelect,
   },
   props: {
     // 时间区间
     value: {
-      type: Array,
+      type: Object,
       required: true,
     },
     // 开始时间的label
     startDateLabel: {
       type: String,
-      default: '开始时间',
+      default: '',
     },
     // 结束时间的label
     endDateLabel: {
       type: String,
-      default: '结束时间',
+      default: '',
     },
     // 选中的日期时间数值的格式
     valueFormat: {
@@ -90,10 +89,6 @@ export default {
       type: String,
       default: 'YYYY-MM-DD',
     },
-    isLink: {
-      type: Boolean,
-      default: true,
-    },
     // 时间类型
     type: {
       type: String,
@@ -107,6 +102,7 @@ export default {
     // 是否显示分隔符
     separatorShow: {
       type: Boolean,
+      default: true,
     },
     // 分隔符
     separator: {
@@ -126,42 +122,87 @@ export default {
       type: Date,
       default: () => dayjs().add(10, 'year').toDate(),
     },
-    readonly: {
-      type: Boolean,
-    },
   },
   data() {
     return {
-      startDate: '',
-      endDate: '',
+      datetimeRange: {
+        startDate: '',
+        endDate: '',
+      }
     };
   },
   computed: {
     minDate() {
-      const startTime = this.startDate || dayjs();
+      const startTime = this.datetimeRange.startDate || dayjs();
       return dayjs(startTime).toDate();
     },
     maxDate() {
-      const endTime = this.endDate || dayjs().add(10, 'year');
+      const endTime = this.datetimeRange.endDate || this.rangeMaxDate;
       return dayjs(endTime).toDate();
     },
   },
   watch: {
     value: {
       deep: true,
-      handler(newValue) {
-        this.startDate = newValue[0];
-        this.endDate = newValue[1];
+      handler() {
+        this.getDateShow();
       },
     },
   },
+  created() {
+    logger.debug('DatetimeRange.created');
+    this.getDateShow();
+  },
   methods: {
+    getDateShow() {
+      this.datetimeRange = clone(this.value);
+    },
+    // 改变开始时间
+    changeStart() {
+      logger.debug('DatetimeRange.changeStart startDate = {0}', this.datetimeRange.startDate);
+      this.change();
+      this.$emit('start', this.datetimeRange.startDate);
+    },
+    // 改变结束时间
+    changeEnd() {
+      logger.debug('DatetimeRange.changeEnd endDate = {0}', this.datetimeRange.endDate);
+      this.change();
+      this.$emit('end', this.datetimeRange.endDate);
+    },
     // 更新时间
-    onConfirm() {
-      console.log('gxw ', [this.startDate, this.endDate])
-      this.$emit('input', [this.startDate, this.endDate]);
+    change() {
+      this.$emit('input', clone(this.datetimeRange));
     }
   },
 };
 </script>
 
+<style lang="less" scoped>
+.datetime-range {
+  display: flex;
+  font-size: 14px;
+  .datetime-range-select  {
+    width: 120px;
+  }
+  /deep/ .van-cell {
+    padding: 0;
+    flex-direction:column;
+  }
+  /deep/ .van-field__label {
+    color: #999;
+  }
+  /deep/ .van-field__value {
+    padding: 0 12px;
+    color: #666;
+    font-size: 14px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 5px;
+  }
+  .separator-wrap {
+    margin: 0 10px;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
